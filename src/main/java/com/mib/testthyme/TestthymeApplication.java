bc958863af837f843f0dbc7f39443c8caed86fe6
mibihi <mibiixi@gmail.com>
2017-07-03 18:14:14 -0500

    first

---
+++ src/main/java/com/mib/testthyme/TestthymeApplication.java
@@ -0,0 +1,12 @@
+package com.mib.testthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestthymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestthymeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mib/testthyme/TestthymeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mib.testthyme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestthymeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
