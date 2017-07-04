49d5dbf57f2ce0d1b531c023344602732b3a7783
mibihi <mibiixi@gmail.com>
2017-07-03 22:36:39 -0500

    2nd bootstrapworks

--- src/main/java/com/mib/testthyme/controllers/TestController.java
+++ src/main/java/com/mib/testthyme/controllers/TestController.java
@@ -1,12 +1,19 @@
 package com.mib.testthyme.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-@GetMapping	
-public String index()
-{ return "index";}
+	// inject via application.properties
+		@Value("${welcome.message:test}")
+		private String message = "Hello World";
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("message", this.message);
+		return "welcome";
+	}
 }
  
